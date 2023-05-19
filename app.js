a294be0bf3c2dd5661b987020e80db42b1b44895
uttrakhand12 <128074024+uttrakhand12@users.noreply.github.com>
2023-05-19 18:18:39 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+    $('.food-slider').slick({
+        autoplay:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        prevArrow:".prev-btn",
+        nextArrow:".next-btn",
+
+        responsive:[
+            {
+                breakpoint:992,
+                settings:{
+                    slidesToShow:2,
+                }
+            }
+        ]
+    });
+
+
+    $('.nav-trigger').click(function(){
+        $('.site-content-wrapper').toggleClass('scaled');
+    })
+});
